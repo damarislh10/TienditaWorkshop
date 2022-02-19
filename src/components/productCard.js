d63b366142dd3c95e369676854ed609a21a821bf
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-02-19 10:47:44 -0500

    changes

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Card, Button} from 'react-bootstrap'
 
-const ProductCard = () => {
+const ProductCard = ({id,name,price,image}) => {
   return (
       <Card style={{ width: '18rem' }}>
         <small>32% dto.</small>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>$26.82/kg <small>$39.9/kg</small></Card.Title>
+          <Card.Title>{price} <small>$39.9/kg</small></Card.Title>
           <Card.Text>
-            limon con semilla
+           {name}
           </Card.Text>
           <Button variant="success">Agregar</Button>
         </Card.Body>
