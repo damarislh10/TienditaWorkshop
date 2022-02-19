6289a2e3b8a1db84c1593d6daf4bb1cd6e7f57e0
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-02-19 10:21:17 -0500

    changes

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { urlProduct } from '../helpers/url'
 
 const ListComponent = () => {
 
   const [product, setProduct] = useState([])
 
   useEffect(() => {
-
+    getData()
   }, [])
 
 
   const getData = async() =>{
-    const resp = await fetch()
+    const resp = await fetch(urlProduct);
+    const data = await resp.json();
+    console.log(data)
   }
   
   return (
--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,1 +1,2 @@
-export const url = ""
+export const urlProduct = "https://data-productos.herokuapp.com/productos1/"
+export const urlPopular = "https://data-productos.herokuapp.com/productos2/"
