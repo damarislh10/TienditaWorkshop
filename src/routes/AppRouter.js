2f9a5bd4dfaf9200578c6657344bea3a4202e9e6
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-02-19 10:06:05 -0500

    Rutas

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,9 +1,17 @@
-
+import ListComponent from "../components/ListComponent";
+import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
+import DetailProduct from "../components/DetailProduct";
 
 function AppRouter() {
   return (
     <div >
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<ListComponent/>}/>
+          <Route path='/product/:id' component={<DetailProduct/>}/>
+          <Route path="*" element={<Navigate to="/"/>}/>  
+        </Routes>
+      </Router>
     </div>
   );
 }
