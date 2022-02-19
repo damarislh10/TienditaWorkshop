7496e610a3bdb7523dac8ecc7750523a357f9c2a
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-02-19 09:43:16 -0500

    changes

---
+++ src/components/ListComponent.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+const ListComponent = () => {
+
+  const [product, setProduct] = useState([])
+
+  useEffect(() => {
+
+  }, [])
+
+
+  const getData = async() =>{
+    const resp = await fetch()
+  }
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListComponent
