d98c0a37041b32b78305dcc782253619997b6a03
Brahian Alzate <brahianalzate02@gmail.com>
2022-02-19 15:42:09 -0500

    home

---
+++ src/containers/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BannerComponent from '../components/BannerComponent'
+import ListComponent from '../components/ListComponent'
+
+const Home = () => {
+  return (
+      <div>
+    <BannerComponent/>
+    <ListComponent/>
+    </div>
+  )
+}
+
+export default Home
